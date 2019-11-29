26d8fb5f2b94448117661b24be286a8d376908eb
Petros <zav.petros@gmail.com>
2019-11-29 15:43:47 +0100

    ResponsiveImage component created. solves #4

--- src/components/vehicle/VehicleDetails.jsx
+++ src/components/vehicle/VehicleDetails.jsx
@@ -4,15 +4,22 @@ import Col from "react-bootstrap/Col";
 import VehicleStatus from "./vehicle-status/VehicleStatus";
 import VehicleInformation from "./vehicle-info/VehicleInformation";
 import FinancialInfo from "./financial-info/FinancialInfo";
+import ResponsiveImage from "../../common/components/responive-image/ResponsiveImage";
 import { VehicleContainer, Header } from "./VehicleDetails.styles";
 import carImg from "../../assets/images/car.jpg";
+import carImg2x from "../../assets/images/car@2x.jpg";
+import carImg3x from "../../assets/images/car@3x.jpg";
 
 const VehicleDetails = ({ vehicle, onChangeStatus }) => {
   return (
     <VehicleContainer>
       <Row>
         <Col xs={12} sm={6} md={4} lg={3} className="mb-3">
-          <img src={carImg} alt={`${vehicle.make} ${vehicle.model}`} />
+          <ResponsiveImage
+            src={carImg}
+            images={[carImg, carImg2x, carImg3x]}
+            alt={`${vehicle.make} ${vehicle.model}`}
+          />
         </Col>
         <Col xs={12} sm={6} md={4} lg={3} className="mb-2">
           <Header>Status</Header>
