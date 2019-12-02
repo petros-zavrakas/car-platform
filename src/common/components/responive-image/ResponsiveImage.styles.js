21871d7cc9bd21bd360b4525c725413478abcb7c
Petros <zav.petros@gmail.com>
2019-12-02 16:02:05 +0100

    Removed unnecessary width property from the Image styles as well as the unnecessary Test styles. resolves

--- src/common/components/responive-image/ResponsiveImage.styles.js
+++ src/common/components/responive-image/ResponsiveImage.styles.js
@@ -4,10 +4,4 @@ export const Image = styled.img.attrs(props => ({
   src: props.src,
   srcSet: props.srcset,
   alt: props.alt
-}))`
-  width: 100%;
-`;
-
-export const Test = styled.div`
-  width: 100%;
-`;
+}))``;
