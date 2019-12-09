9e4f1dd54d7f06fd4e5c5392747196450ce49789
Petros <zav.petros@gmail.com>
2019-12-09 19:39:58 +0100

    Implement GeneralErrorBoundery component with user-friendly display

---
+++ src/components/errors/error-boundary/GeneralErrorBoundary.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import AlertBox from "../../alert-box/AlertBox.test";
+
+class GeneralErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    });
+  }
+
+  render() {
+    const { errorInfo, error } = this.state;
+
+    if (errorInfo) {
+      return (
+        <AlertBox variant="danger">
+          <p>
+            <h4>Something went wrong! </h4>
+            <span>{error && error.toString()}</span>
+          </p>
+        </AlertBox>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default GeneralErrorBoundary;
